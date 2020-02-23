77fc442a3b9229ab9efbc02e23228cd2c8b1ddf7
Vladimir Mikhaylov <vovencia@gmail.com>
2020-02-23 17:30:32 +0300

    Added docs

---
+++ docs/precache-manifest.2b3d8fca4c26d442609df2c520f42745.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195d1c2d7920405bb4c1da307adc75c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eec80b47d852b55069e7",
+    "url": "./static/css/main.88d41a79.chunk.css"
+  },
+  {
+    "revision": "d5b3cec42d41b25a3e89",
+    "url": "./static/js/2.eb1648ef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.eb1648ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec80b47d852b55069e7",
+    "url": "./static/js/main.33c686bb.chunk.js"
+  },
+  {
+    "revision": "94106d998be695b99224",
+    "url": "./static/js/runtime-main.9c7924e9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
